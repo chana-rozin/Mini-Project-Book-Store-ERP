73cb540b41640e46f9b249cdb1059839282bb76b
chana-rozin <c0556787069@gmail.com>
2024-10-09 15:33:31 +0300

    first init

---
+++ model.js
@@ -0,0 +1,76 @@
+const Gbooks = [
+    {
+        id: 1,
+        title: "JavaScript: The Good Parts",
+        price: 29.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/81kqrwS1nNL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 2,
+        title: "Eloquent JavaScript",
+        price: 24.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/91asIC1fRwL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 3,
+        title: "You Don't Know JS",
+        price: 34.99,
+        rate: 4,
+        imgUrl: "https://m.media-amazon.com/images/I/71mKvDODaSL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 4,
+        title: "Clean Code",
+        price: 39.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 5,
+        title: "The Pragmatic Programmer",
+        price: 49.99,
+        rate: 4,
+        imgUrl: "https://m.media-amazon.com/images/I/51A5cHQhSqL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 6,
+        title: "Design Patterns: Elements of Reusable Object-Oriented Software",
+        price: 54.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/91P1aR7tVGL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 7,
+        title: "Introduction to Algorithms",
+        price: 79.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/81vJybG2YHL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 8,
+        title: "Refactoring: Improving the Design of Existing Code",
+        price: 45.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/51kL7mKEziL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 9,
+        title: "The Mythical Man-Month: Essays on Software Engineering",
+        price: 39.99,
+        rate: 4,
+        imgUrl: "https://m.media-amazon.com/images/I/41MxsBDgZDL._AC_UY327_FMwebp_QL65_.jpg"
+    },
+    {
+        id: 10,
+        title: "Code Complete: A Practical Handbook of Software Construction",
+        price: 49.99,
+        rate: 5,
+        imgUrl: "https://m.media-amazon.com/images/I/51-SZ8aLHoL._AC_UY327_FMwebp_QL65_.jpg"
+    }
+];
+
+console.log("dump",Gbooks);
+
+const GdynamicBooks = []
