7ded4a1b96c02d913db409c49b8e89ea75621675
chana-rozin <c0556787069@gmail.com>
2024-10-28 14:34:40 +0200

    fix the dumb to contain working urls

--- model.js
+++ model.js
@@ -38,72 +38,74 @@ const Gbooks = [
         title: "JavaScript: The Good Parts",
         price: 29.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/81kqrwS1nNL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/8091016-L.jpg"
     },
     {
         id: 2,
         title: "Eloquent JavaScript",
         price: 24.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/91asIC1fRwL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/10528254-L.jpg"
     },
     {
         id: 3,
         title: "You Don't Know JS",
         price: 34.99,
         rate: 4,
-        imgUrl: "https://m.media-amazon.com/images/I/71mKvDODaSL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/10528075-L.jpg"
     },
     {
         id: 4,
         title: "Clean Code",
         price: 39.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/8592224-L.jpg"
     },
     {
         id: 5,
         title: "The Pragmatic Programmer",
         price: 49.99,
         rate: 4,
-        imgUrl: "https://m.media-amazon.com/images/I/51A5cHQhSqL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/10482338-L.jpg"
     },
     {
         id: 6,
         title: "Design Patterns: Elements of Reusable Object-Oriented Software",
         price: 54.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/51kuc24SxSL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/8231996-L.jpg"
     },
     {
         id: 7,
         title: "Introduction to Algorithms",
         price: 79.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/71Fh6hDqAtL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/13518232-L.jpg"
     },
     {
         id: 8,
         title: "Refactoring: Improving the Design of Existing Code",
         price: 45.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/41+N6chblcL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/8275068-L.jpg"
     },
     {
         id: 9,
         title: "The Mythical Man-Month: Essays on Software Engineering",
         price: 39.99,
         rate: 4,
-        imgUrl: "https://m.media-amazon.com/images/I/51kNtlWx5lL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/8168310-L.jpg"
     },
     {
         id: 10,
         title: "Code Complete: A Practical Handbook of Software Construction",
         price: 49.99,
         rate: 5,
-        imgUrl: "https://m.media-amazon.com/images/I/81b9Bw3W-jL._AC_UY327_FMwebp_QL65_.jpg"
+        imgUrl: "https://covers.openlibrary.org/b/id/8231990-L.jpg"
     }
 ];
+
+
 
 console.log("dump",Gbooks);
 
